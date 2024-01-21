6ecf5214ecba823bc0b19d577c210c4f4632fa77
Siddarth Satish <120223529+Sidd-satish12@users.noreply.github.com>
2024-01-20 21:39:44 -0500

    Update additional_functions.py

--- Backend Functions/additional_functions.py
+++ Backend Functions/additional_functions.py
@@ -106,3 +106,20 @@ def query_1build_construction_costs(api_key,lat,lng):
 
     return results
 
+def sustainability_score(data):
+    score = 75
+    if data['solar']:
+        score += 25
+
+    if data['water_check']:
+        score -= 15
+
+    pollution_num = 50
+    while data['pollution'] > pollution_num:
+        score -= 2
+        pollution_num += 30
+    
+    return score
+# solar and water_check are boolean vals
+# pollution is average pollution over x amount of days
+
